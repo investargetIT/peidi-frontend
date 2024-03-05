0f6db728cc2c0ba7dc040cb3b691d9f1acc63419
Oliver <olimovier@gmail.com>
2024-03-05 16:18:22 +0800

    Update index page UI

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <div style="position: relative;">
     <van-image style="position: absolute;left: 0;top: 0;" width="100vw" height="100vh" fit="cover" position="center" src="/WechatIMG633.jpg" />
     <div style="position: relative;padding-top: 100px;">
-      <div style="text-align: center;margin-bottom: 20px;font-size: 24px;">点击下方，为宠物发声！</div>
-      <NuxtLink to="/register"><van-button style="width: 50%;margin: auto;" block type="primary">加入</van-button></NuxtLink>
+      <!-- <div style="text-align: center;margin-bottom: 20px;font-size: 24px;">点击下方，为宠物发声！</div> -->
+      <NuxtLink to="/register"><van-button style="width: 50%;margin: auto;" block type="primary">点击按钮，为宠物发声！</van-button></NuxtLink>
     </div>
   </div>
 </template>
