7c030f88137be8ab9288c9e3fa37dddb7af0fb11
Oliver <olimovier@gmail.com>
2024-03-08 11:50:52 +0800

    Coupon Link

--- pages/success.vue
+++ pages/success.vue
@@ -6,7 +6,8 @@
 
 <template>
   <van-image style="position: relative;" width="100vw" src="/bg.jpg" />
-  <div style="position: absolute;top: 100px; width: 100%;">
+  <div style="position: absolute;top: 50px; width: 100%;">
     <div style="width: 80%;margin:auto;text-align: center;background-color: orange;color: white;">感谢您参与发声公益活动！<br>送您一份天猫专属旗舰店优惠！</div>
+    <div style="width: 80%;margin: 20px auto;text-align: center;background-color: orange;color: white;word-break: break-all;">https://taoquan.taobao.com/coupon/unify_apply.htm?sellerId=2206358157998&activityId=3b9dbf5e57224bf98944143cc244cb5c&toolName=shopCoupon<br>请复制链接并在浏览器中打开</div>
   </div>
 </template>
