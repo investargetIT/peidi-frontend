fc3de677d1b3d32c9017b7d73ec23d61be76715b
Oliver <olimovier@gmail.com>
2024-03-08 15:50:09 +0800

    Update success page

--- pages/success.vue
+++ pages/success.vue
@@ -7,11 +7,11 @@
 <template>
   <van-image style="position: relative;" width="100vw" src="/bg.jpg" />
   <div style="position: absolute;top: 50px; width: 100%;">
-    <div style="width: 80%;margin:auto;text-align: center;background-color: orange;color: white;">感谢您参与发声公益活动！<br>送您一份天猫专属旗舰店优惠！</div>
-    <div style="width: 80%;margin: 20px auto;text-align: center;background-color: orange;color: white;word-break: break-all;">https://taoquan.taobao.com/coupon/unify_apply.htm?sellerId=2206358157998&activityId=3b9dbf5e57224bf98944143cc244cb5c&toolName=shopCoupon<br>请复制链接并在浏览器中打开</div>
+    <div style="width: 80%;margin:auto;text-align: center;color: white;"><span style="background-color: orange;">感谢您参与发声公益活动！<br>送您一份天猫专属旗舰店优惠！</span></div>
+    <div style="width: 80%;margin: 20px auto;text-align: center;color: white;word-break: break-all;"><span style="background-color: orange;">https://taoquan.taobao.com/coupon/unify_apply.htm?sellerId=2206358157998&activityId=3b9dbf5e57224bf98944143cc244cb5c&toolName=shopCoupon<br>请复制链接并在浏览器中打开</span></div>
     <van-row justify="center">
       <van-image width="100" src="/qr.png" />
     </van-row>
-    <div style="width: 80%;margin: 20px auto;text-align: center;background-color: orange;color: white;">扫码添加小蒂，关注活动后续进展以及优惠券使用，还有更多品牌福利等你解锁！</div>
+    <div style="width: 80%;margin: 20px auto;text-align: center;color: white;"><span style="background-color: orange;">扫码添加小蒂，关注活动后续进展以及优惠券使用，还有更多品牌福利等你解锁！</span></div>
   </div>
 </template>
