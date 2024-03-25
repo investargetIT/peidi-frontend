9170c308d051e44ff6dee17e034384a43be7523a
Oliver <olimovier@gmail.com>
2024-03-25 10:02:03 +0800

    Update dd logon free page

---
+++ pages/peidi/dd.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { initDingH5RemoteDebug } from "dingtalk-h5-remote-debug";
+  import * as dd from 'dingtalk-jsapi'; // 此方式为整体加载，也可按需进行加载
+
+  initDingH5RemoteDebug();
+  const runtimeConfig = useRuntimeConfig();
+  dd.runtime.permission.requestAuthCode({
+    corpId: runtimeConfig.public.DINGTALK_CORP_ID, // 企业id
+    onSuccess: function (info) {
+      console.log('info', info);
+      const { code } = info; // 通过该免登授权码可以获取用户身份
+      $fetch('https://api.investarget.com/service/weixin/pduserinfo', {
+        method: 'POST',
+        body: { code },
+      }).then((res) => {
+        console.log('res', res);
+        if (res.code === 1000) {
+          localStorage.setItem('ddUserInfo', JSON.stringify(res.result));
+        }
+      });
+    },
+  });
+</script>
