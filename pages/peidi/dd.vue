952d682eecbacfeb24bcd131435e31e147de4a40
Oliver <olimovier@gmail.com>
2024-03-25 10:34:09 +0800

    Catch dd log free error

--- pages/peidi/dd.vue
+++ pages/peidi/dd.vue
@@ -7,17 +7,22 @@
   dd.runtime.permission.requestAuthCode({
     corpId: runtimeConfig.public.DINGTALK_CORP_ID, // 企业id
     onSuccess: function (info) {
-      console.log('info', info);
+      console.log(info);
       const { code } = info; // 通过该免登授权码可以获取用户身份
       $fetch(runtimeConfig.public.API_BASE_URL + '/service/dinguserinfo', {
         method: 'POST',
         body: { code },
       }).then((res) => {
-        console.log('res', res);
+        console.log(res);
         if (res.code === 1000) {
           localStorage.setItem('ddUserInfo', JSON.stringify(res.result));
         }
+      }).catch((err) => {
+        console.log(err);
       });
+    },
+    onFail: function(err) {
+      console.log(err);
     },
   });
 </script>
