2993bcff8d01ced8f6b7c4683006b3828ba33631
Oliver <olimovier@gmail.com>
2024-07-10 09:54:39 +0800

    引入表格组件 easy table

--- pages/echarts.vue
+++ pages/echarts.vue
@@ -65,6 +65,13 @@
     <div style="flex: 1;text-align: center;">{{ item[5] }}</div>
   </div>
 
+  <div>
+    <EasyDataTable
+      :headers="headers"
+      :items="items"
+    />
+  </div>
+
   <h2 style="text-align: center;">客服数据</h2>
   <h3 style="text-align: center">1.部分店铺DSR</h3>
   <div id="chart_shop_dsr" style="width: 100%;height: 300px;"></div>
@@ -81,6 +88,22 @@ export default {
       orderThirtyDaysData: null,
       orderYearData: null,
       stockData: [],
+      headers: [
+        { text: "PLAYER", value: "player" },
+        { text: "TEAM", value: "team" },
+        { text: "NUMBER", value: "number" },
+        { text: "POSITION", value: "position" },
+        { text: "HEIGHT", value: "indicator.height" },
+        { text: "WEIGHT (lbs)", value: "indicator.weight", sortable: true },
+        { text: "LAST ATTENDED", value: "lastAttended", width: 200 },
+        { text: "COUNTRY", value: "country" },
+      ],
+      items: [
+        { player: "Stephen Curry", team: "GSW", number: 30, position: 'G', indicator: { "height": '6-2', "weight": 185 }, lastAttended: "Davidson", country: "USA" },
+        { player: "Lebron James", team: "LAL", number: 6, position: 'F', indicator: { "height": '6-9', "weight": 250 }, lastAttended: "St. Vincent-St. Mary HS (OH)", country: "USA" },
+        { player: "Kevin Durant", team: "BKN", number: 7, position: 'F', indicator: { "height": '6-10', "weight": 240 }, lastAttended: "Texas-Austin", country: "USA" },
+        { player: "Giannis Antetokounmpo", team: "MIL", number: 34, position: 'F', indicator: { "height": '6-11', "weight": 242 }, lastAttended: "Filathlitikos", country: "Greece" },
+      ],
     };
   },
   mounted() {
